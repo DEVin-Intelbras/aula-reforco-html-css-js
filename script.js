f516c68c50dcd043192fe26b1648a93d58e026ee
Michael Nascimento <michael.nsc@outlook.com>
2022-07-12 22:05:57 -0300

    Exemplos desenvolvidos em sala

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-var lista = document.getElementById("lista");
-var campoTarefa = document.getElementById("titulo-tarefa");
-var botaoAdicionar = document.getElementById("adicionar");
+// Sintaxe de array
 
-botaoAdicionar.addEventListener("click", function () {
-  var novaTarefa = campoTarefa.value;
-  lista.innerHTML += "<li>" + novaTarefa + "</li>";
-  campoTarefa.value = "";
-  campoTarefa.focus();
-});
+var alunos = ["Brian", "Diego", "Otavio", "Karla", "Julia"];
 
-// ...
+var stringAlunos = alunos.join(" ");
+console.log(stringAlunos);
+
+var nomeDoProfessor = "Michael Guilherme de Oliveira Nascimento";
+var nomesDoProfessor = nomeDoProfessor.split(" ");
+console.log(nomesDoProfessor);
+
+// Sintaxe de objeto
+var carro = {
+  cor: "Prata",
+  modelo: "Hatch",
+  marca: "BMW",
+  placa: "A1B2C3",
+  ano: 2022,
+  tetoSolar: true,
+  motor: "v8",
+};
+
+var carro2 = {
+  cor: "Preta",
+  modelo: "Sedan",
+  marca: "Mercedez",
+  placa: "A1B2C3",
+  ano: 1984,
+  tetoSolar: false,
+  motor: "v8",
+  foiDeletado: true,
+};
+
+var listaCarros = [carro, carro2];
+
+console.log(listaCarros);
