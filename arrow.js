7b209c616476a33a4dc679bde643cba10e10eb8b
Michael Nascimento <michael.nsc@outlook.com>
2022-07-15 19:11:32 -0300

    Exercicios em aula

---
+++ arrow.js
@@ -0,0 +1,57 @@
+// funcao nomeada
+// function multiplicarPor2(numero) {
+//   return numero * 2;
+// }
+
+// funcao callback
+// document.addEventListener("click", function () {
+//   console.log("");
+// });
+
+// funcao anonima atribuida a variavel
+// var funcaoSoma = function (params) {};
+
+//
+// var mostrarLog = () => {
+//   console.log("Minha arrow function");
+// };
+
+// const multiplicarPor2 = num => num * 2;
+
+// const resultado = multiplicarPor2(5);
+// console.log(resultado);
+
+// const minhaFnCallback =  () => {
+//   console.log("")
+// };
+
+// function minhaFn() {}
+
+// document.addEventListener("click", ()=> {});
+
+const listaNomes = ["Karla", "André", "Douglas", "Julia"]
+
+var listaNumeros = [1, 2, 3, 4, 5]
+
+// const listaAlunos = [
+//   {nome: "Karla", sobrenome: "Ciesielski"},
+//   {nome: "André", sobrenome: "Victor"},
+//   {nome: "Douglas", sobrenome: "Luis"},
+//   {nome: "Julia", sobrenome: "Perdona"},
+// ]
+
+const listaAlunos = [
+  {nome: "Karla", sobrenome: "Ciesielski"},
+  {nome: "André", sobrenome: "Victor"},
+  {nome: "Douglas", sobrenome: "Luis"},
+  {nome: "Julia", sobrenome: "Perdona"},
+]
+
+listaAlunos.forEach(aluno => console.log(aluno.nome + " " + aluno.sobrenome ))
+
+
+
+// console.log(listaResultante)
+
+
+
